a943d58a8d7e3c14111e97ced44d74eb4334182c
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-06-02 23:21:30 +0100

    added user service and dto

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @InjectRepository(User)
+  private readonly userRepo: Repository<User>;
+
+  public getUser(id: string): Promise<User> {
+    return this.userRepo.findOne({ where: { id } });
+  }
+
+  public createUser(body: CreateUserDto): Promise<User> {
+    const newUser: User = this.userRepo.create(body);
+
+    return this.userRepo.save(newUser);
+  }
+}
